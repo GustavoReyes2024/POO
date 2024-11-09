02d9f741c0eac3623301b0cce6f46eb746e8ae57
USER <USER@DESKTOP-N8VH6UI>
2024-11-09 15:17:22 -0600

    Actualizacion de la Clase Perro

--- src/main/java/POO/Herencia.java
+++ src/main/java/POO/Herencia.java
@@ -4,19 +4,31 @@
 
 package POO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author USER
  */
 public class Herencia {
 
+    
+    
     public static void main(String[] args) {
        Animal p = new Perro("color negro", "si", "si", "si", 4);
-       p.Sonido();
+       //p.Sonido();
        
        Animal g = new Gato("Blanco", "si", "si", "si", 4);
-       g.Sonido();
+       //g.Sonido();
        
+       List<Animal> lista = new ArrayList<>();
+       lista.add(p);
+       lista.add(g);
+       lista.add(new Quequo("no","si", "si", "si", 4));
        
+       for(Animal item : lista){
+           item.Sonido();
+       }
     }
 }
